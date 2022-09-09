699cb6030114fbe339e3486b5bda42ae0c3c933c
mcpeck <peckmelodie@gmail.com>
2022-09-08 20:07:33 -0500

    Pass tests up to objects in stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // I: primitive value, could be collection (object or array)
+  // O: stringified version of the input
+  // C:
+  // E: undefined or function will be omitted and return null
+
+
+  var type = typeof obj;
+
+  // if input is number, string, boolean or null, return stringified version
+
+  if (type === 'number' || type === 'boolean') {
+    return '' + obj;
+  } else if (type === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return 'null';
+  }
+
+  if (Array.isArray(obj) === true) {
+    var arrResult = [];
+    for (var i = 0; i < obj.length; i++) {
+      // result += stringifyJSON(obj[i]) + ', ';
+      arrResult.push(stringifyJSON(obj[i]));
+    }
+    return '[' + (arrResult.join(',')) + ']';
+  }
+
+  // if input is an object, iterate through it and recursively call the function on each value within it
+  // same as above for an arrays
+
+
 };
