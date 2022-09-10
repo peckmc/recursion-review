2e694c094dfefe869b46d9642a8d7bf9af1cfbcd
mcpeck <peckmelodie@gmail.com>
2022-09-10 14:13:00 -0500

    Complete Bare Minimum Requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,37 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// I - a document (webpage)
+// O - an array of elements that match the input class name
+// C - use recursion
+// E - N/A
+
+
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // delcare variable respresenting document
+  var body = document.body;
+  // declare result array variable
+  var result = [];
+
+  // create child function that takes in an 'element' parameter
+  var findNodes = function (node) {
+
+    var classList = node.classList;
+    var children = node.childNodes;
+
+    if (classList && classList.contains(className)) {
+      result.push(node);
+    }
+
+    if (children) {
+      for (var i = 0; i < children.length; i++) {
+        findNodes(children[i]);
+      }
+    }
+  }
+  // call child functon on document
+  findNodes(body);
+  return result;
 };
